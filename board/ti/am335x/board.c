fa2c091243c6b4a9306ad7ef19eb750b4aa2eb2e
Paul Barker <paul.barker@sancloud.com>
2021-08-14 15:57:14 +0100

    board: ti: am335x: Add support for BBE Extended WiFi
    The Sancloud BeagleBone Enhanced Extended WiFi (BBE Extended WiFi) has
    its own devicetree file and the board can be identified by the 2nd
    letter of the config string within the common EEPROM.
    
    Signed-off-by: Paul Barker <paul.barker@sancloud.com>

--- board/ti/am335x/board.c
+++ board/ti/am335x/board.c
@@ -827,10 +827,16 @@ int board_late_init(void)
 		name = "BBG1";
 	if (board_is_bben()) {
 		char subtype_id = board_ti_get_config()[1];
-		if (subtype_id == 'L')
-			name = "BBELITE";
-		else
-			name = "BBEN";
+		switch (subtype_id) {
+			case 'L':
+				name = "BBELITE";
+				break;
+			case 'I':
+				name = "BBE_EX_WIFI";
+				break;
+			default:
+				name = "BBEN";
+		}
 	}
 	set_board_info_env(name);
 
