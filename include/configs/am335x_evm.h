9e2b52381cdbe26971595cd3538dc6c61b1a19ff
hosseinf <hossein.farkhonde@sancloud.com>
2026-04-27 11:56:04 +0100

    add conf_root for banner

--- include/configs/am335x_evm.h
+++ include/configs/am335x_evm.h
@@ -134,8 +134,9 @@
     "boot_secureFIT=" \
 		"if test \"$secure_fit_filename\" = \"SanCloud-SecureBOOT-image.fit\"; then " \
             "echo setting bootargs for normal boot...; " \
-            "setenv bootargs console=ttyS0,115200n8 root=/dev/mmcblk${devnum}p${part} ro rootfstype=ext4 rootwait coherent_pool=1M net.ifnames=0 lpj=1990656 rng_core.default_quality=100 quiet; " \
+            "setenv bootargs console=ttyS0,115200n8 root=/dev/mmcblk${devnum}p${part} ro rootfstype=ext4 rootwait coherent_pool=1M net.ifnames=0 lpj=1990656 rng_core.default_quality=100; " \
         "fi; " \
+		"setenv bootargs \"${bootargs} conf_root=/dev/mmcblk${devnum}p${part}  quiet\";"\
         "load ${devtype} ${devnum}:${part} ${addr_fit} ${bootdir}/${secure_fit_filename}; " \
         "echo Booting ${bootdir}/${secure_fit_filename} from ${devtype} ${devnum}:${part} ...; " \
         "echo bootargs=${bootargs}; " \
